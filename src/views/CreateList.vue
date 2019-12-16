e7aed8eb50260e0e2f5cd921afa17dd9ad8398fe
wonglok <yellowhappy831@gmail.com>
2019-12-17 00:21:16 +0800

    back backup

--- src/views/CreateList.vue
+++ src/views/CreateList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative mx-auto max-w-sm">
+  <div class="relative mx-auto rem24 max-w-sm">
     <!-- Title -->
     <div class="mt-2 mx-4 text-2xl font-bold py-2 border-b border-gray-200">
       Craft Essential Message
@@ -7,7 +7,7 @@
 
     <!-- Content -->
     <div class="mx-4 my-4" :key="i" v-for="(content, i) in contentTypes">
-      <img class="cursor-pointer w-full image-presize" :src="content.src" :alt="content.alt">
+      <img class="cursor-pointer w-full" :src="content.src" :alt="content.alt">
       <p class="cursor-pointer my-3">
         {{ content.alt }}
       </p>
@@ -61,7 +61,7 @@ export default {
 .img-r25{
   width: 100%;
 }
-.image-presize{
-  min-height: calc(8rem);
+.rem24{
+  width: 24rem;
 }
 </style>
