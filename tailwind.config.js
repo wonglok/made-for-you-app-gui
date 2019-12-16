f42d5f624ce6f93dcd49666fdcc2ba22401a5574
wonglok <yellowhappy831@gmail.com>
2019-12-16 09:21:00 +0800

    back backup

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        title: [
+          'Eczar',
+          'Georgia',
+          'Cambria',
+          '"Times New Roman"',
+          'Times',
+          'serif'
+        ],
+        mono: [
+          'RobotoMono-Medium',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          '"Liberation Mono"',
+          '"Courier New"',
+          'monospace'
+        ],
+        sans: [
+          'Avenir',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"'
+        ],
+        serif: [
+          'Poppins',
+          'Georgia',
+          'Cambria',
+          '"Times New Roman"',
+          'Times',
+          'serif'
+        ]
+      }
+    }
+  },
+  variants: {
+  },
+  plugins: []
+}
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+// const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+
+module.exports = {
+  devServer: {
+    // proxy: {
+    //   // netlify dev with functions
+    //   '/.netlify/functions/': {
+    //     target: 'http://localhost:8888'
+    //   }
+    // }
+  },
+  css: {
+    extract: false
+  },
+  configureWebpack: {
+    plugins: [
+      // new MonacoWebpackPlugin()
+    ]
+  }
+}
