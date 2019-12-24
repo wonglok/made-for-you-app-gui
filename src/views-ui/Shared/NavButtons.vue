4e3670d3495e11f7199bf9de9c0705959b93ad22
wonglok <yellowhappy831@gmail.com>
2019-12-24 13:13:57 +0800

    nav buttons

--- src/views-ui/Shared/NavButtons.vue
+++ src/views-ui/Shared/NavButtons.vue
@@ -21,7 +21,7 @@
       </div>
     </a>
     <router-link to="/connect">
-      <div class=" cursor-pointer mx-6 px-8 py-4 text-lg font-title rounded-full bg-brand-primary text-white hover:bg-purple-400 transition-bg transition-500">
+      <div class="cursor-pointer mx-6 px-8 py-4 text-lg font-title rounded-full bg-brand-primary text-white hover:bg-purple-400 transition-bg transition-500">
         <span class="select-none">Register / Login</span>
       </div>
     </router-link>
