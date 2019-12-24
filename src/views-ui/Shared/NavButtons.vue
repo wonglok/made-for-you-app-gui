737d9efde3b79824fcf954d152bfad3ca9f1ab67
wonglok <yellowhappy831@gmail.com>
2019-12-24 13:05:07 +0800

    save

--- src/views-ui/Shared/NavButtons.vue
+++ src/views-ui/Shared/NavButtons.vue
@@ -15,20 +15,19 @@
         Gallery
       </div>
     </router-link>
-    <!-- <a target="_blank" exact-active-class="font-bold " href="https://github.com/wonglok/made-for-you-app-gui" to="/gallery" >
+    <a target="_blank" exact-active-class="font-bold " href="https://github.com/wonglok/made-for-you-app-gui" to="/gallery" >
       <div class="mx-6 hover:underline  cursor-pointer">
         Github ↗️
       </div>
-    </a> -->
+    </a>
     <div class=" cursor-pointer mx-6 px-8 py-4 text-lg font-title font-black rounded-full bg-brand-primary text-white hover:bg-purple-400 transition-bg transition-500">
-      <span class="select-none">Login</span>
+      <span class="select-none">Register / Login</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
 }
 </script>
 
