7edd31aaf89c2a42c3f049bfa50a77aaa341add4
wonglok <yellowhappy831@gmail.com>
2020-01-26 18:54:53 +0800

    back backup

--- src/views-ui/Shared/FooterSection.vue
+++ src/views-ui/Shared/FooterSection.vue
@@ -22,9 +22,6 @@
           <div class="text-md">
             Our Story
           </div>
-          <div class="text-md">
-            <a target="_blank" href="https://github.com/wonglok/made-for-you-app-gui">Github ↗️</a>
-          </div>
         </div>
         <div class="lg:w-3/12">
           <div class="title-height text-xl font-title font-black tracking-tighter text-black">Credits</div>
@@ -43,7 +40,6 @@
         <a target="_blank" href="https://twitter.com/WongLok831"><img class="p-3" src="../../assets/icons/twitter.svg" alt="Twitter"></a>
         <a target="_blank" href="https://www.instagram.com/wonglok831/"><img class="p-3" src="../../assets/icons/ig.svg" alt="Instagram"></a>
         <a target="_blank" href="https://www.facebook.com/okWealthy/"><img class="p-3" src="../../assets/icons/fb.svg" alt="Facebook"></a>
-        <a target="_blank" href="https://github.com/wonglok/made-for-you-app-gui"><img class="p-3" src="../../assets/icons/github.svg" alt="Github"></a>
       </div>
     </div>
   </div>
