d530556ce4b2e5cfe25d56b32de7646386f6c8a5
wonglok <yellowhappy831@gmail.com>
2020-01-28 08:21:59 +0800

    back backup

--- src/views-ui/Home/DemoSection.vue
+++ src/views-ui/Home/DemoSection.vue
@@ -4,13 +4,13 @@
       <div class="px-6 pb-12 flex flex-col lg:flex-row">
         <div class="lg:w-full lg:order-1">
           <div class="lg:px-12">
-            <div class=" font-title font-bold text-xl tracking-tight text-red-500">
-              Assisted Creative Coding
+
+            <div class="py-4 pb-0 font-title font-bold text-xl tracking-tight text-red-500">
+              Speed things up....
             </div>
             <div class="py-4 font-title font-black text-5xl tracking-tighter leading-none text-black">
-              Editor System
+              Assisted Creative Coding
             </div>
-
             <!-- <div class=" text-lg leading-relaxed py-3">
               To celebrate the Gift of Life, I try to build hard things. Nice things are hard to be here, but it's always worth it.
             </div>
@@ -21,7 +21,6 @@
             <div class="pb-6">
               <img class="border border-gray-300 shadow-2xl rounded-lg" src="./img/editor.png" alt="">
             </div>
-
           </div>
         </div>
       </div>
