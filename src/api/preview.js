161fa0c9f664abd02230307bf98f4c915c18860c
wonglok <yellowhappy831@gmail.com>
2020-01-28 08:54:56 +0800

    back backup

--- src/api/preview.js
+++ src/api/preview.js
@@ -25,6 +25,41 @@ export const makePreviewer = async ({ app, mounter, previewPageKey }) => {
   }
   let routes = []
 
+  /* eslint-disable */
+  // long version
+  function loadExt (files, after) {
+    var _this=this;
+    _this.files = files;
+    _this.js = [];
+    _this.head = document.getElementsByTagName("head")[0];
+    _this.after = after || function(){};
+    _this.loadStyle = function(file) {
+      var link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.type = "text/css";
+      link.href = file;
+      _this.head.appendChild(link);
+    };
+    _this.loadScript = function(i) {
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = _this.js[i];
+      var loadNextScript = function() {
+        if (++i < _this.js.length) _this.loadScript(i);
+        else _this.after();
+      };
+      script.onload = function() { loadNextScript() };
+      _this.head.appendChild(script);
+    }
+    for (var i=0;i<_this.files.length;i++) {
+      if (/\.js$|\.js\?/.test(_this.files[i])) _this.js.push(_this.files[i])
+      if (/\.css$|\.css\?/.test(_this.files[i])) _this.loadStyle(_this.files[i])
+    }
+    if (_this.js.length>0) _this.loadScript(0);
+    else _this.after();
+  }
+  /* eslint-enable */
+
   let initCode = (mods, modItem, code, MyModules, setupPromiseTasks) => {
     let env = {
       get moduleName () {
